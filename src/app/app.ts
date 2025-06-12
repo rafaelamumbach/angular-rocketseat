a7884362d8c9e058483eb630b8299428eba989e6
Rafaela Mumbach Buenos <rafaela.buenos@totvs.com.br>
2025-06-12 15:01:22 -0300

    Componentes - criação da interface base do usuário

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,11 @@ import { Navbar } from "./_components/navbar/navbar";
 import { PrimaryButton } from "./primary-button/primary-button";
 import { SecondaryButton } from "./_components/secondary-button/secondary-button";
 import { ItemCertificado } from "./_components/item-certificado/item-certificado";
+import { BaseUi } from "./_components/base-ui/base-ui";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, PrimaryButton, SecondaryButton, ItemCertificado],
+  imports: [RouterOutlet, Navbar, PrimaryButton, SecondaryButton, ItemCertificado, BaseUi],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

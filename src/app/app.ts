a990597f1bb63259f42baa06bda3424599f1bdb2
Rafaela Mumbach Buenos <rafaela.buenos@totvs.com.br>
2025-06-12 13:37:14 -0300

    Componentes - aulas iniciais

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from "./_components/navbar/navbar";
+import { PrimaryButton } from "./primary-button/primary-button";
+import { SecondaryButton } from "./_components/secondary-button/secondary-button";
+import { ItemCertificado } from "./_components/item-certificado/item-certificado";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, PrimaryButton, SecondaryButton, ItemCertificado],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'gerador-certificado';
+  exibeNav: boolean = true;
 }

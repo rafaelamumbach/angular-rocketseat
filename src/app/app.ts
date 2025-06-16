80f12e2606020903ee6e6ca49d0f34d32a52cc61
Rafaela Mumbach Buenos <rafaela.buenos@totvs.com.br>
2025-06-16 09:32:13 -0300

    Pages - Criação de componente base e alterações gerais

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,10 +5,11 @@ import { PrimaryButton } from "./primary-button/primary-button";
 import { SecondaryButton } from "./_components/secondary-button/secondary-button";
 import { ItemCertificado } from "./_components/item-certificado/item-certificado";
 import { BaseUi } from "./_components/base-ui/base-ui";
+import { Certificados } from "./pages/certificados/certificados";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, PrimaryButton, SecondaryButton, ItemCertificado, BaseUi],
+  imports: [RouterOutlet, Navbar, PrimaryButton, SecondaryButton, ItemCertificado, BaseUi, Certificados],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/pages/certificados/certificados.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-certificados',
+  imports: [],
+  templateUrl: './certificados.html',
+  styleUrl: './certificados.css'
+})
+export class Certificados {
+
+}

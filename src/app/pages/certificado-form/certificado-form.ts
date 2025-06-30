1377866840056881a191a93fcdd37273d4d0105a
Rafaela Mumbach Buenos <rafaela.buenos@totvs.com.br>
2025-06-30 15:39:30 -0300

    Pages - Criação e estilização da div de atividades. Mudanças gerais nos botões e continuação do desenvolvimento geral.

--- src/app/pages/certificado-form/certificado-form.ts
+++ src/app/pages/certificado-form/certificado-form.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { PrimaryButton } from "../../_components/primary-button/primary-button";
+import { SecondaryButton } from "../../_components/secondary-button/secondary-button";
 
 @Component({
   selector: 'app-certificado-form',
-  imports: [],
+  imports: [PrimaryButton, SecondaryButton],
   templateUrl: './certificado-form.html',
   styleUrl: './certificado-form.css'
 })
--- src/app/pages/certificados/certificados.ts
+++ src/app/pages/certificados/certificados.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SecondaryButton } from "../../_components/secondary-button/secondary-button";
+import { ItemCertificado } from "../../_components/item-certificado/item-certificado";
 
 @Component({
   selector: 'app-certificados',
-  imports: [SecondaryButton],
+  imports: [SecondaryButton, ItemCertificado],
   templateUrl: './certificados.html',
   styleUrl: './certificados.css'
 })

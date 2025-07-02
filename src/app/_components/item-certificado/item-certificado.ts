ef5a7f7847af2598f89ef6fcda176f9c46751cbc
Rafaela Mumbach Buenos <rafaela.buenos@totvs.com.br>
2025-07-02 13:47:26 -0300

    Rotas - Utilização de routerLink para interatividade/navegação entre páginas

--- src/app/_components/item-certificado/item-certificado.ts
+++ src/app/_components/item-certificado/item-certificado.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { SecondaryButton } from "../secondary-button/secondary-button";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-item-certificado',
-  imports: [SecondaryButton],
+  imports: [SecondaryButton, RouterLink],
   templateUrl: './item-certificado.html',
   styleUrl: './item-certificado.css'
 })
 export class ItemCertificado {
-
+  id: string = '1'
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { Certificados } from './pages/certificados/certificados';
+import { CertificadoForm } from './pages/certificado-form/certificado-form';
+import { Certificado } from './pages/certificado/certificado';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: Certificados
+    },
+
+    {
+        path: "certificados",
+        component: Certificados
+    },
+
+    {
+        path: "certificados/novo",
+        component: CertificadoForm
+    },
+
+    {
+        path: "certificados/:id",
+        component: Certificado
+    },
+
+];

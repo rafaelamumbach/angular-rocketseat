03afeaf905f54fd142a644e8b9ff2d92301a86ef
Rafaela Mumbach Buenos <rafaela.buenos@totvs.com.br>
2025-07-04 11:19:52 -0300

    Geral | Botão dinâmico para gerar certificado

--- src/app/_components/primary-button/primary-button.ts
+++ src/app/_components/primary-button/primary-button.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-primary-button',
+  standalone: true,
   imports: [],
   templateUrl: './primary-button.html',
   styleUrl: './primary-button.css'
 })
 export class PrimaryButton {
-
+  @Input() textoBotao: string = '';
 }

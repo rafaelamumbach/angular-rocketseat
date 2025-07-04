df809387948c4c19d3ad531a6ca626b46199bb81
Rafaela Mumbach Buenos <rafaela.buenos@totvs.com.br>
2025-07-04 14:34:15 -0300

    Formulario - Deixando a lista de atividades dinâmica e tratando de condições de input

--- src/app/_components/primary-button/primary-button.ts
+++ src/app/_components/primary-button/primary-button.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-primary-button',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './primary-button.html',
   styleUrl: './primary-button.css'
 })
 export class PrimaryButton {
   @Input() textoBotao: string = '';
+  @Input() disabled: boolean = false;
 }
